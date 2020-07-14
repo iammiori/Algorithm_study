5255da2eb8c12b608b93abf476aabeae70a035d3
iammiori <mioriii@naver.com>
2020-07-15 04:27:55 +0900

    [xcode]

---
+++ ProblemSolving/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProblemSolving
+//
+//  Created by miori Lee on 2020/07/15.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
