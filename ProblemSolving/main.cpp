39b30c5ae2767770e7097e39933484934891ee8c
iammiori <mioriii@naver.com>
2020-07-15 04:38:23 +0900

    [feat] add namespace

--- ProblemSolving/main.cpp
+++ ProblemSolving/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
