99e267a3190cdba895044cdb6e16f91d56a2f6f6
iammiori <mioriii@naver.com>
2020-11-06 16:30:17 +0900

    [modify] combination,set활용버전

--- programmers/level01/q3_pick_and_add.py
+++ programmers/level01/q3_pick_and_add.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
 # 첫번째 풀이
@@ -14,8 +15,23 @@ def solution(numbers):
 	answer.sort()
 	return answer
 
+def solution2(numbers):
+	answer = []
+	l = list(combinations(numbers,2))
+	#print(l)
+	for i in l:
+		answer.append(i[0]+i[1])
+
+	# set을 활용한 중복제거
+	answer = list(set(answer))
+	answer.sort()
+
+	return answer
+
+
 if __name__ == '__main__':
 	arr = list(map(int,input().split()))
 	print(solution(arr))
+	print(solution2(arr))
 
 
