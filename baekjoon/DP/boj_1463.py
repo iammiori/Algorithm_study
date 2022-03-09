7951616076f0508d58fd9e7b5928863fc308041f
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-03-09 16:23:22 +0900

    [220309] boj_1463 : dp - bottom up - O(N) (#15)

--- baekjoon/DP/boj_1463.py
+++ baekjoon/DP/boj_1463.py
@@ -1,11 +1,15 @@
 import sys
 input = sys.stdin.readline
 
+# bottom up으로 구현
 def makeOne(num):
     for i in range (2, N+1) :
+        # -1 하는 경우
         memoization[i] = memoization[i-1] + 1 
+        # 3으로 나누는 경우
         if i%3 == 0 :
             memoization[i] = min(memoization[i],memoization[i//3]+1)
+        # 2로 나누는 경우
         if i%2 == 0 :
             memoization[i] = min(memoization[i],memoization[i//2]+1)
     return memoization[num]
@@ -13,5 +17,6 @@ def makeOne(num):
 
 N = int(input())
 memoization = [0] * (N+1)
+# 가장 작은 크기의 문제 답 필수!
 memoization[1] = 0
 print(makeOne(N))
