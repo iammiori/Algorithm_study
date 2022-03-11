fd0b97e4959b2d7ac068f20f7c9a4b7a1feb8277
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-03-11 22:36:48 +0900

    [220311] fix : index error 수정 (boj 11727 타일2)

--- baekjoon/DP/boj_11727.py
+++ baekjoon/DP/boj_11727.py
@@ -4,7 +4,8 @@ input = sys.stdin.readline
 def makeTile2(n):
     # 초기
     arr[1] = 1
-    arr[2] = 3
+    if n >= 2 :
+        arr[2] = 3
     for i in range(3,n+1) :
         # 점화식
         arr[i] = (arr[i-2] * 2) + arr[i-1]
