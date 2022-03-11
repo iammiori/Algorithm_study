3a930d55784a34af877fab211508d4bb2d3a3dd5
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-03-11 22:26:57 +0900

    [220311] boj_11727 : dp  타일링2 (#18)

---
+++ baekjoon/DP/boj_11727.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def makeTile2(n):
+    # 초기
+    arr[1] = 1
+    arr[2] = 3
+    for i in range(3,n+1) :
+        # 점화식
+        arr[i] = (arr[i-2] * 2) + arr[i-1]
+    return arr[-1]%10007
+        
+N = int(input())
+arr = [0] * (N+1)
+print(makeTile2(N))
