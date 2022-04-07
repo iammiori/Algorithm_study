2806770534ec0a83621531fdb01202bd9441fc64
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-04-08 01:00:55 +0900

    [fix] 틀린이유 : 여벌 체육복 갖고온 친구가 도난 당한 경우 고려 안함
    - set 으로 중복값 제거

--- programmers/level01/python/gymclothes.py
+++ programmers/level01/python/gymclothes.py
@@ -1,4 +1,13 @@
+# def solution(n, lost, reserve):
+#     answer = n - len(lost)
+#     for r in reversed(reserve) :
+#         if r-1 in lost or r+1 in lost :
+#             answer += 1
+#             lost.pop()
+#     return answer
+
 def solution(n, lost, reserve):
+    lost, reserve = list(set(lost) - set(reserve)), list(set(reserve) - set(lost))
     answer = n - len(lost)
     for r in reversed(reserve) :
         if r-1 in lost or r+1 in lost :
